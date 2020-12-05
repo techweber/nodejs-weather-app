7e07a13f57c5381114cdb3b8ae3170cad688411e
Usman <umahmood@gmail.com>
2020-12-05 14:47:23 +0500

    Update weatherapp.js

--- weatherapp.js
+++ weatherapp.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 
-let api_key = 'e845dfea671332a1e942ea5fec4ab12a';
+let api_key = '<your_api_key_from_openweathermap.org';
 let city = 'london';
 let api_url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`;
 fetch( api_url )
